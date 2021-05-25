592b222ba46ea0eb6ed9ba54ca070dc4eb27a6f9
Nehari <mohwebs@gmail.com>
2021-05-25 20:52:48 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Mi Card'),
+        ),
+        body: Container(
+          
+        ),
+      ),
+    ),
+  );
+}
